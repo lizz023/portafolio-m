8a814e3a67091b5aecc91b90b48fe14b12cd27e9
Elizabeth Restrepo <elizabeth.restrepo@grupoconsultic.com.mx>
2025-02-07 16:47:05 -0500

    Agrego Link a imagenes de proyectos

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,40 +1,60 @@
 import styled from "styled-components";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css"
-import ImgTriqui from "../../src/assets/Img/encriptador.png"
+import ImgEncriptador from "../../src/assets/Img/encriptador.png"
+import ImgTicTacToe from "../../src/assets/Img/tictactoe.png"
+import ImgComming from "../../src/assets/Img/comming.jpg"
+import { Link } from "react-router-dom";
 
 const ProjectsContainer = styled.div`
     align-items: center;
     background: #202020;
     color: white;
     display: flex;
-    height: 50vh;
+    height: 100vh;
     margin: 0 auto;
 `
 // const ImgProjects = styled.img`
 //     width: 100%;
 //     padding: 15px;
+//     img:rec
 // `
 
 const Projects = () => {
 
     const images = [
         {
-            original: ImgTriqui, thumbnail:ImgTriqui
-            
+            original: ImgEncriptador, 
+            thumbnail:ImgEncriptador,
+            link: "https://lizz023.github.io/challenge/"
         },
         {
-            original: ImgTriqui, thumbnail:ImgTriqui
+            original: ImgTicTacToe, 
+            thumbnail:ImgTicTacToe,
+            link: "https://lizz023.github.io/triquiliz/"
         },
         {
-            original: ImgTriqui, thumbnail:ImgTriqui
+            original: ImgComming, 
+            thumbnail:ImgComming,
+
         },
     ];
+
+    const renderItem = (item) => (
+        <Link to={item.link} style={{ display: "block" }} target="blank">
+          <img 
+            src={item.original} 
+            alt="Project" 
+            style={{ width: "100%", height: "auto", borderRadius: "10px" }}
+          />
+        </Link>
+    );
 
     return(
         <ProjectsContainer>
             <ImageGallery 
                 items={images}
+                renderItem={renderItem}
                 showFullscreenButton={false}
                 showBullets={true}
                 showPlayButton={false}
